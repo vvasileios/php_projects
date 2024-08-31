9c9cc701fc9547bc04b466f8ac36ae4a0d0ee297
Vasileios <vtvasileiadis@gmail.com>
2024-08-31 23:49:25 +0300

    set up html/css structure

---
+++ tic-tac-toe/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Tic-Tac-Toe</title>
+</head>
+
+<body>
+</body>
+<div class="container">
+    <h1 class="title">Tic-Tac-Toe</h1>
+    <div class="board">
+        <div class="cell" id="0"></div>
+        <div class="cell" id="1"></div>
+        <div class="cell" id="2"></div>
+        <div class="cell" id="3"></div>
+        <div class="cell" id="4"></div>
+        <div class="cell" id="5"></div>
+        <div class="cell" id="6"></div>
+        <div class="cell" id="7"></div>
+        <div class="cell" id="8"></div>
+    </div>
+    <button class="action-btn">Start</button>
+</div>
+
+</html>
