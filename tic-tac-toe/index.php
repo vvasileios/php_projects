1aea9e2711af9ac0467bd2ae4cabae3e220a8351
Vasileios <vtvasileiadis@gmail.com>
2024-09-03 23:51:25 +0300

    minor

--- tic-tac-toe/index.php
+++ tic-tac-toe/index.php
@@ -9,21 +9,21 @@
 </head>
 
 <body>
-</body>
-<div class="container">
-    <h1 class="title">Tic-Tac-Toe</h1>
-    <div class="board">
-        <div class="cell" id="0"></div>
-        <div class="cell" id="1"></div>
-        <div class="cell" id="2"></div>
-        <div class="cell" id="3"></div>
-        <div class="cell" id="4"></div>
-        <div class="cell" id="5"></div>
-        <div class="cell" id="6"></div>
-        <div class="cell" id="7"></div>
-        <div class="cell" id="8"></div>
+    <div class="container">
+        <h1 class="title">Tic-Tac-Toe</h1>
+        <div class="board">
+            <div class="cell" id="0"></div>
+            <div class="cell" id="1"></div>
+            <div class="cell" id="2"></div>
+            <div class="cell" id="3"></div>
+            <div class="cell" id="4"></div>
+            <div class="cell" id="5"></div>
+            <div class="cell" id="6"></div>
+            <div class="cell" id="7"></div>
+            <div class="cell" id="8"></div>
+        </div>
+        <button class="action-btn">Start</button>
     </div>
-    <button class="action-btn">Start</button>
-</div>
+</body>
 
 </html>
