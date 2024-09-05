9ff081280d8badf92e7d5a415831a02abf70bd35
Vasileios <vtvasileiadis@gmail.com>
2024-09-05 21:44:21 +0300

    add destroy session to clear data / re-order file

--- tic-tac-toe/logic.php
+++ tic-tac-toe/logic.php
@@ -1,29 +1,6 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['start'])) {
-    $_SESSION['board'] = array_fill(0, 9, '');
-    $_SESSION['current_player'] = 'X';
-}
-
-$currentPlayer = $_SESSION['current_player'];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['board'])) {
-    $board = $_SESSION['board'];
-    $currentPlayer = $_SESSION['current_player'];
-
-    foreach ($_POST['board'] as $index => $value) {
-        if ($board[$index] === '') {
-            $board[$index] = $currentPlayer;
-            $_SESSION['board'] = $board;
-            $_SESSION['current_player'] = $currentPlayer === 'X' ? 'O' : 'X';
-            break;
-        }
-    }
-
-    $currentPlayer = $_SESSION['current_player'];
-}
-
 function checkWinner($board, $player)
 {
     $winningCombos = [
@@ -56,6 +33,29 @@ function isDisabled($board)
     return checkWinner($board, 'X') || checkWinner($board, 'O') || isFull($board);
 }
 
+if (!isset($_SESSION['start'])) {
+    $_SESSION['board'] = array_fill(0, 9, '');
+    $_SESSION['current_player'] = 'X';
+}
+
+$currentPlayer = $_SESSION['current_player'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['board'])) {
+    $board = $_SESSION['board'];
+    $currentPlayer = $_SESSION['current_player'];
+
+    foreach ($_POST['board'] as $index => $value) {
+        if ($board[$index] === '') {
+            $board[$index] = $currentPlayer;
+            $_SESSION['board'] = $board;
+            $_SESSION['current_player'] = $currentPlayer === 'X' ? 'O' : 'X';
+            break;
+        }
+    }
+
+    $currentPlayer = $_SESSION['current_player'];
+}
+
 if (isset($_POST['start'])) {
     $_SESSION['start'] = true;
     $board = $_SESSION['board'];
@@ -63,6 +63,7 @@ if (isset($_POST['start'])) {
 
 if (isset($_POST['reset'])) {
     session_unset();
+    session_destroy();
     header('Location: index.php');
     exit();
 }
